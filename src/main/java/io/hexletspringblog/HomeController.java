1caa7d6dbbe60ed405b2dca11e40504d4fc050fe
Levasey <164722696+Levasey@users.noreply.github.com>
2025-09-21 06:52:32 +0600

    Add Application and home controller

--- src/main/java/io/hexletspringblog/HomeController.java
+++ src/main/java/io/hexletspringblog/HomeController.java
@@ -1,4 +1,13 @@
 package io.hexletspringblog;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Добро пожаловать в Hexlet Spring Blog!";
+    }
 }
