70dfcea7106cf1fb40a9b7edd49fc86528becb34
Levasey <164722696+Levasey@users.noreply.github.com>
2025-10-24 17:08:28 +0600

    Add about.html, index.html

--- src/test/java/io/hexletspringblog/controller/HomeControllerTest.java
+++ src/test/java/io/hexletspringblog/controller/HomeControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,13 +25,17 @@ class HomeControllerTest {
     void home_returnsWelcomeMessage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Добро пожаловать в Hexlet Spring Blog!"));
+                .andExpect(view().name("index"))
+                .andExpect(content().string(containsString("Welcome to Hexlet!")))
+                .andExpect(content().string(containsString("This is simple Spring blog!")));
     }
 
     @Test
     void about_returnsAboutMessage() throws Exception {
         mockMvc.perform(get("/about"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("This is simple Spring blog!"));
+                .andExpect(view().name("about"))
+                .andExpect(content().string(containsString("About Us")))
+                .andExpect(content().string(containsString("This is a simple Spring blog created for Hexlet project")));
     }
 }
